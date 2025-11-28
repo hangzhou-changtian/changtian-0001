728045845f2fd070e976293666530546ec75dff2
shawn <xionchen@foxmail.com>
2025-11-28 16:30:58 +0800

    更新数据

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/changtian-no1/',
+  base: '/changtian-0001/',
 })
